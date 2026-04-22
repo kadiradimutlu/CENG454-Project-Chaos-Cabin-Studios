7abc452925b14525b08f04c66c66a085c55c9a42
Osman Bilgin <efman_su@hotmail.com>
2026-04-22 21:56:58 +0300

    feat: Player script is implemented

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+
+		
+	}
+
+}
