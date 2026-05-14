9b7d9b65f692b97e0eb8162b98c1998add081307
Mustafa Esad Sarp <s200444081@stu.thk.edu.tr>
2026-05-15 01:50:47 +0300

    feat/changed-for-rigidbody

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,45 +1,63 @@
 using Fusion;
 using UnityEngine;
-
-
-
+ 
+ 
+ 
 public class PlayerMovement : NetworkBehaviour
 {
-    private bool isMovementAllowed;
-
+    private bool isMovementAllowed = true;
+ 
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotationSpeed = 10f;
-
-    private CharacterController _controller;
-    private void Awake() {
-    _controller = GetComponent<CharacterController>();
-}
+    [SerializeField] private float jumpForce = 5f;
+   
+ 
+    private Rigidbody _rb;
+    private void Awake()
+    {
+    _rb = GetComponent<Rigidbody>();
+    }
     public override void FixedUpdateNetwork()
     {
         if (GetInput(out GameplayInput input))
         {
-            
+           
             var direction = input.MoveDirection.normalized;
-
-            DoMove(direction);
+ 
+            DoMove(direction, input);
         }
     }
-
-    private void DoMove(Vector2 direction)
-{
-    if (isMovementAllowed)
+ 
+    private void DoMove(Vector2 direction, GameplayInput input)
     {
-    Vector3 moveVector = new Vector3(direction.x, 0, direction.y);
-
-    
-    transform.position += moveVector * moveSpeed * Runner.DeltaTime;
-
-    
-    if (moveVector != Vector3.zero)
+        if (isMovementAllowed)
+        {
+            Vector3 moveVector = new Vector3(direction.x, 0, direction.y);
+ 
+            float currentSpeed = moveSpeed;
+            if (input.SprintButton) currentSpeed = moveSpeed * 2f;
+            else if (input.CrouchButton) currentSpeed = moveSpeed * 0.5f;
+   
+            _rb.MovePosition(_rb.position + moveVector * currentSpeed * Runner.DeltaTime);
+ 
+            if (input.JumpButton && IsGrounded())
+            {
+                _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+ 
+   
+            if (moveVector != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(moveVector);
+                _rb.MoveRotation(Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Runner.DeltaTime));
+            }
+           
+        }
+    }
+ 
+    private bool IsGrounded()
     {
-        Quaternion targetRotation = Quaternion.LookRotation(moveVector);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Runner.DeltaTime);
+        return Physics.Raycast(transform.position, Vector3.down, 1.1f);
     }
 }
-}
-}
+ 
